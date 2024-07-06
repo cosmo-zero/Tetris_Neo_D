cec57c653741e805441700737cf6bca5fae87847
cosmo-zero <110287972+cosmo-zero@users.noreply.github.com>
2024-07-06 13:13:54 +0900

    Create style.css

---
+++ style.css
@@ -0,0 +1,195 @@
+
+:root {
+  --color-bg: #000000;
+  --color-text-main: #000000;
+  --color-primary: #FFFF00;
+  --wrapper-height: 100vh;
+  --image-max-width: 500px;
+  --image-margin: 3rem;
+  --font-family: "HK Grotesk";
+  --font-family-header: "HK Grotesk";
+}
+
+/* Basic page style resets */
+* {
+  box-sizing: border-box;
+}
+[hidden] {
+  display: none !important;
+}
+
+/* Import fonts */
+@font-face {
+  font-family: HK Grotesk;
+  src: url("https://cdn.glitch.me/605e2a51-d45f-4d87-a285-9410ad350515%2FHKGrotesk-Regular.otf?v=1603136326027")
+    format("opentype");
+}
+@font-face {
+  font-family: HK Grotesk;
+  font-weight: bold;
+  src: url("https://cdn.glitch.me/605e2a51-d45f-4d87-a285-9410ad350515%2FHKGrotesk-Bold.otf?v=1603136323437")
+    format("opentype");
+}
+
+/* Our remix on glitch button */
+.btn--remix {
+  font-family: HK Grotesk;
+  padding: 0.75rem 1rem;
+  font-size: 1.1rem;
+  line-height: 1rem;
+  font-weight: 500;
+  height: 2.75rem;
+  align-items: center;
+  cursor: pointer;
+  background: #FFFFFF;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 4px;
+  text-decoration: none;
+  color: #000000;
+  white-space: nowrap;
+  margin-left: auto;
+  margin-bottom: px;
+}
+
+.btn--remix img {
+  margin-right: 0.5rem;
+}
+.btn--remix:hover {
+  background-color: #D0FFF1;
+}
+
+
+.divider {
+  padding: 0 1rem;
+}
+/******************************************************************************
+END Glitch hello-app default styles
+******************************************************************************/
+
+body {
+  font-family: HK Grotesk;
+  background: linear-gradient(135deg, #69F7BE, #1e53ff);
+}
+
+/* Page structure */
+.wrapper {
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  margin: 0 1rem;
+}
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+/* Very light scaling for our illustration */
+.title {
+  color: #FFFFFF;
+  font-family: HK Grotesk;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 100px;
+  line-height: 105%;
+  margin: 0;
+  line-height: 1; /* 行の高さを調整 */
+  margin: -70px 0 0 0; /* 上部にマイナスのマージンを追加 */
+}
+
+/* Very light scaling for our illustration */
+.illustration {
+  width: 400px; /* 新しい幅をピクセル単位で設定 */
+  height: 400px; /* 新しい高さをピクセル単位で設定 */
+  margin-top: var(--image-margin);
+}
+
+/* Instructions */
+.instructions {
+  margin: 1rem auto 0;
+}
+
+/* Button - Add it from the README instructions */
+button,
+input {
+  font-family: inherit;
+  font-size: 100%;
+  background: #FFFFFF;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 4px;
+  padding: 0.5rem 1rem;
+  transition: 500ms;
+}
+
+.button-container {
+  margin-top: 2rem; /* 任意の値に調整してください */
+}
+
+
+.button-container button {
+  background-color: #5500cc; /* ボタンの背景色 */
+  color: #FFFFFF; /* テキストの色 */
+  border: 1; /* ボタンの枠線を消す */
+  border-color: #2757a1; /* ボタンの枠線の色 */
+}
+
+.button-container:active {
+  transform: translateY(2px);
+}
+
+/* Subheading */
+h1 {
+  color: #eefcff;
+  text-align: center; /* テキストを中央揃いにする */
+}
+h2 {
+  color: #eefcff;
+  text-align: center; /* テキストを中央揃いにする */
+}
+h3 {
+  color: #eefcff;
+  text-align: center; /* テキストを中央揃いにする */
+}
+
+/* Interactive image */
+.illustration:active {
+  /*transform: translateY(5px);*/
+}
+
+/* Button dip
+- Toggling this class on and off will move it down and up again
+- The button transition property above determines the speed of the translate (500ms)
+*/
+
+/* Links that open files in the editor */
+.fileopener {
+  cursor:pointer;
+  font-weight:bold;
+  border-bottom:3px solid var(--color-primary);
+  color: var(--color-secondary);
+}
+.fileopener:hover {
+  border-bottom:3px solid var(--color-secondary);
+}
+
+
+
+.input-container {
+  display: flex;
+  justify-content: center;
+  align-items: center; /* 入力ボックスを垂直方向に中央揃え */
+  flex-direction: column; /* 入力ボックスを縦に並べる */
+}
+
+.input-column {
+  margin: -10px; /* 適切なマージンを追加してください */
+  text-align: center; /* 入力ボックスを中央に揃える */
+}
+
+/* 入力ボックスの横幅を調整 */
+.input-column input {
+  width: 300px; /* 任意の幅に調整してください */
+}
